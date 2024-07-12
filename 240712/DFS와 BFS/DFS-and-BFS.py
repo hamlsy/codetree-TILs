0563d1cddd31edbe37c174343bde59808190e034
Lee seung young <70877744+hamlsy@users.noreply.github.com>
2024-07-12 15:15:15 +0900

    Title: DFS와 BFS, Time: 161ms, Memory: 26MB, Status: Wrong Answer - CodeTree

---
+++ 240712/DFS와 BFS/DFS-and-BFS.py
@@ -0,0 +1,39 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+n, m, num = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+dfs_result = []
+v = [False] * (n+1)
+
+def dfs(x, v):
+    dfs_result.append(x)
+    v[x] = True
+    for node in graph[x]:
+        if v[node] == False:        
+            dfs(node, v)
+
+dfs(num ,v)
+print(" ".join(map(str, dfs_result)))
+
+
+bfs_result = []
+v = [False] * (n+1)
+queue = deque()
+queue.append(num)
+
+while queue:
+    x = queue.popleft()
+    if v[x] == False:
+        v[x] = True
+        bfs_result.append(x)
+        for i in graph[x]:
+            queue.append(i)
+
+print(" ".join(map(str, bfs_result)))
